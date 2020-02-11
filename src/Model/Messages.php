60b0b59b961d12e2599ce1063f2ac18faf3212aa
Florian Leroux <florian.leroux3@laposte.net>
2020-02-11 09:55:43 +0100

    Rerouting Messages

--- src/Model/Messages.php
+++ src/Model/Messages.php
@@ -1,10 +1,12 @@
 <?php
+
 namespace src\Model;
 
 use src\Model\Bdd;
 
-Class Messages{
-    
+Class Messages
+{
+
 
     private $ID_MESSAGE;
     private $ID_UTILISATEUR;
@@ -14,17 +16,19 @@ Class Messages{
     private $MES_LU;
 
 
-    public function afficherContact($campus){
+    public function afficherContact($campus)
+    {
 
 
         $listUser = (new Utilisateur)->SqlGetBy(Bdd::GetInstance(),
-        'SELECT * FROM UTILISATEUR WHERE UTI_CAMPUS =:param', $campus);
+            'SELECT * FROM UTILISATEUR WHERE UTI_CAMPUS =:param', $campus);
 
         return $listUser;
 
     }
 
-    public function selectMesParId($idMessage){
+    public function selectMesParId($idMessage)
+    {
 
         $bdd = Bdd::GetInstance();
         $requete = $bdd->prepare("SELECT MES_CONTENU
@@ -34,22 +38,24 @@ Class Messages{
         $reponse = $requete->fetch();
         return $reponse;
     }
-    
-    public function afficherAncienMsg($userid, $contactid){
+
+    public function afficherAncienMsg($userid, $contactid)
+    {
 
         $bdd = Bdd::GetInstance();
         $afficher = $bdd->prepare("SELECT * FROM MESSAGE
         WHERE ID_UTILISATEUR LIKE ? OR ID_UTILISATEUR LIKE  ?
         AND UTI_ID_UTILISATEUR LIKE ? OR UTI_ID_UTILISATEUR LIKE ?
         ORDER BY MES_DATE LIMIT 50;");
-        $afficher->execute(array($userid,$contactid,$contactid,$userid));
+        $afficher->execute(array($userid, $contactid, $contactid, $userid));
         $msg = $afficher->fetchAll();
         $afficher->closeCursor();
 
         return $msg;
     }
 
-    public function envoyerMsg($userid, $contactid, $msg){
+    public function envoyerMsg($userid, $contactid, $msg)
+    {
 
         $bdd = Bdd::GetInstance();
         $envoyer = $bdd->prepare("INSERT INTO `MESSAGE` (`ID_UTILISATEUR`, `UTI_ID_UTILISATEUR`, `MES_CONTENU`) 
@@ -58,52 +64,16 @@ Class Messages{
             'userid' => $userid,
             'contactid' => $contactid,
             'contenu' => $msg
-            ]);
+        ]);
         return $rSql;
-
 
 
     }
 
 
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     /**
      * Get the value of ID_MESSAGE
-     */ 
+     */
     public function getID_MESSAGE()
     {
         return $this->ID_MESSAGE;
@@ -113,7 +83,7 @@ Class Messages{
      * Set the value of ID_MESSAGE
      *
      * @return  self
-     */ 
+     */
     public function setID_MESSAGE($ID_MESSAGE)
     {
         $this->ID_MESSAGE = $ID_MESSAGE;
@@ -123,7 +93,7 @@ Class Messages{
 
     /**
      * Get the value of ID_UTILISATEUR
-     */ 
+     */
     public function getID_UTILISATEUR()
     {
         return $this->ID_UTILISATEUR;
@@ -133,7 +103,7 @@ Class Messages{
      * Set the value of ID_UTILISATEUR
      *
      * @return  self
-     */ 
+     */
     public function setID_UTILISATEUR($ID_UTILISATEUR)
     {
         $this->ID_UTILISATEUR = $ID_UTILISATEUR;
@@ -143,7 +113,7 @@ Class Messages{
 
     /**
      * Get the value of UTI_ID_UTILISATEUR
-     */ 
+     */
     public function getUTI_ID_UTILISATEUR()
     {
         return $this->UTI_ID_UTILISATEUR;
@@ -153,7 +123,7 @@ Class Messages{
      * Set the value of UTI_ID_UTILISATEUR
      *
      * @return  self
-     */ 
+     */
     public function setUTI_ID_UTILISATEUR($UTI_ID_UTILISATEUR)
     {
         $this->UTI_ID_UTILISATEUR = $UTI_ID_UTILISATEUR;
@@ -163,7 +133,7 @@ Class Messages{
 
     /**
      * Get the value of MES_DATE
-     */ 
+     */
     public function getMES_DATE()
     {
         return $this->MES_DATE;
@@ -173,7 +143,7 @@ Class Messages{
      * Set the value of MES_DATE
      *
      * @return  self
-     */ 
+     */
     public function setMES_DATE($MES_DATE)
     {
         $this->MES_DATE = $MES_DATE;
@@ -183,7 +153,7 @@ Class Messages{
 
     /**
      * Get the value of MES_CONTENU
-     */ 
+     */
     public function getMES_CONTENU()
     {
         return $this->MES_CONTENU;
@@ -193,7 +163,7 @@ Class Messages{
      * Set the value of MES_CONTENU
      *
      * @return  self
-     */ 
+     */
     public function setMES_CONTENU($MES_CONTENU)
     {
         $this->MES_CONTENU = $MES_CONTENU;
@@ -203,7 +173,7 @@ Class Messages{
 
     /**
      * Get the value of MES_LU
-     */ 
+     */
     public function getMES_LU()
     {
         return $this->MES_LU;
@@ -213,7 +183,7 @@ Class Messages{
      * Set the value of MES_LU
      *
      * @return  self
-     */ 
+     */
     public function setMES_LU($MES_LU)
     {
         $this->MES_LU = $MES_LU;
